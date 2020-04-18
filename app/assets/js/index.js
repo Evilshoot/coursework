f748618f2bae4b7cc886b37bc3e7cc6013b90e71
Maksym Striganov <38066347+Evilshoot@users.noreply.github.com>
2020-04-19 01:28:57 +0300

    in progress

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -6,7 +6,7 @@ function parse() {
     month: 'long',
     year: 'numeric'
   }
-  fetch("https://api.apify.com/v2/datasets/QnpvWy7yH1w0PAMkS/items?format=json&clean=1")
+  fetch("/dataset_my-task-2.json")
     .then(response => {
       if (response.status !== 200)
         throw new Error("Not 200 response")
@@ -19,6 +19,12 @@ function parse() {
     }).then(data => {
     data.posts.map((post) => {
       let counter = 0
+      let counter2 = 0
+      let counter3 = 0
+      let counter4 = 0
+      let counter5 = 0
+      let counter6 = 0
+      let counter7 = 0
       return $(".parser__handler").append(`<div class="parser__handler-item">` +
         `<div class="parser__handler-item__date">` + new Date(post.postDate).toLocaleDateString('uk-UA', dateOptions) + `</div>` +
         `<div class="parser__handler-item__stats">` +
@@ -29,8 +35,72 @@ function parse() {
         `<div class="parser__handler-item__words">` +
         `<div class="parser__handler-item__words-match">` + `США: `
         + post.postComments.comments.map((comment, i) => {
-          if (JSON.stringify(comment.text).includes('США') || JSON.stringify(comment.text).includes('Сша') || JSON.stringify(comment.text).includes('сша')) {
+          if (JSON.stringify(comment.text).toLowerCase().includes('сша')) {
             counter++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Пентагон: `
+        + post.postComments.comments.map((comment, i) => {
+
+          if (JSON.stringify(comment.text).toLowerCase().includes('пентагон')) {
+            counter2++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter2
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Коронавірус: `
+        + post.postComments.comments.map((comment, i) => {
+          if (JSON.stringify(comment.text).toLowerCase().includes('коронавірус') || JSON.stringify(comment.text).toLowerCase().includes('коронавирус')) {
+            counter3++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter3
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Росія: `
+        + post.postComments.comments.map((comment, i) => {
+          if (JSON.stringify(comment.text).toLowerCase().includes('росія') || JSON.stringify(comment.text).toLowerCase().includes('россия')) {
+            counter4++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter4
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Криза: `
+        + post.postComments.comments.map((comment, i) => {
+          if (JSON.stringify(comment.text).toLowerCase().includes('криза') || JSON.stringify(comment.text).toLowerCase().includes('кризис')) {
+            counter5++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter5
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Карантин: `
+        + post.postComments.comments.map((comment, i) => {
+          if (JSON.stringify(comment.text).toLowerCase().includes('карантин') || JSON.stringify(comment.text).toLowerCase().includes('карантин')) {
+            counter6++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter6
+          }
+        }).join('') +
+        `</div>` +
+        `<div class="parser__handler-item__words-match">` + `Вірус: `
+        + post.postComments.comments.map((comment, i) => {
+          if (JSON.stringify(comment.text).toLowerCase().includes('вірус') || JSON.stringify(comment.text).toLowerCase().includes('вирус')) {
+            counter7++
+          }
+          if(post.postComments.comments.length - 1 === i){
+            return counter7
           }
         }).join('') +
         `</div>` +
